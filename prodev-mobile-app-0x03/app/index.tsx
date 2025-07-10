a662d79c36221ec9139e6bbe84d887d6443d50f3
Vegas90 <amosmwoni@gmail.com>
2025-07-10 16:44:31 +0300

    amos

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -1,7 +1,7 @@
 import { Text, TextInput, View, TouchableOpacity, Image} from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
-import styles from "@/styles/index";
+import { styles } from "@/styles/index";
 
 
 export default function Index() {
