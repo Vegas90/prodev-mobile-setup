67307a613476d386a0385ea4944bc5dd90dc6294
Vegas90 <amosmwoni@gmail.com>
2025-07-10 16:38:30 +0300

    m

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -71,33 +71,30 @@ export default function Index() {
   <SafeAreaView style={{ flex: 1 }}>
    <ImageBackground
    // Set a full-screen background image using ImageBackground with resizeMode=“cover” and ensure the background image covers the entire screen using Dimensions.get(“window”).height
-  source={require("@/assets/images/background-image.png")}
-  style={styles.background}
-  resizeMode="cover"
+    source={require("@/assets/images/background-image.png")}
+    style={styles.background}
+    resizeMode="cover"
   >
-  <View style={styles.companyLogo}>
-  <Image source={require("@/assets/images/Logo.png")} />
-</View>
-<View style={styles.textGroup}>
-  <Text style={styles.textLarge}>Find your favorite place here</Text>
-  <Text style={styles.textSmall}>The best prices for over 2 </Text>
-  <Text style={styles.textSmall}>million properties worldwide</Text>
-</View>
-<View style={styles.buttonGroup}>
-  <TouchableOpacity style={styles.button}>
-    <Text style={{ ...styles.textSmall, color: "black" }}>Join here</Text>
-  </TouchableOpacity>
-
-  <TouchableOpacity style={styles.transparentButton}>
-    <Text style={styles.textSmall}>Sign In</Text>
-  </TouchableOpacity>
-</View>
-
-<View style={{ alignItems: "center", paddingVertical: 20 }}>
-  <Text style={{ color: "white" }}>Continue to home</Text>
-</View>
-
-</ImageBackground>
+    <View style={styles.companyLogo}>
+      <Image source={require("@/assets/images/Logo.png")} />
+      </View>
+      <View style={styles.textGroup}>
+        <Text style={styles.textLarge}>Find your favorite place here</Text>
+        <Text style={styles.textSmall}>The best prices for over 2 </Text>
+        <Text style={styles.textSmall}>million properties worldwide</Text>
+        </View>
+        <View style={styles.buttonGroup}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={{ ...styles.textSmall, color: "black" }}>Join here</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.transparentButton}>
+              <Text style={styles.textSmall}>Sign In</Text>
+              </TouchableOpacity>
+              </View>
+              <View style={{ alignItems: "center", paddingVertical: 20 }}>
+                <Text style={{ color: "white" }}>Continue to home</Text>
+                </View>
+      </ImageBackground>
   </SafeAreaView>
 </SafeAreaProvider>
 
